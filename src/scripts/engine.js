d558e99dcea1ca09293bca8a8ffa5e17d31c4986
flp96 <filipefernandes__@live.com>
2024-12-02 11:45:49 -0300

    hitbox listener implementado

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -8,7 +8,9 @@ const state = {
 
   values: {
     timerId: null,
-    gameVelocity: 1000
+    gameVelocity: 1000,
+    hitPosition: 0,
+    result: 0,
   }
 }
 
@@ -20,6 +22,7 @@ function randomSquare() {
   let randomNumber = Math.floor(Math.random() * 9);
   let randomSquare = state.view.squares[randomNumber];
   randomSquare.classList.add("enemy");
+  state.values.hitPosition = randomSquare.id;
 }
 
 function moveEnemy() {
@@ -28,11 +31,19 @@ function moveEnemy() {
 
 function addListenerHitbox() {
   state.view.squares.forEach((square) => {
-  })
+    square.addEventListener("mousedown", () => {
+      if(square.id === state.values.hitPosition) {
+        state.values.result++;
+        state.view.score.textContent = state.values.result;
+        state.values.hitPosition = null;
+      }
+    })
+  });
 }
 
 function initialize(){
   moveEnemy();
+  addListenerHitbox();
 };
 
 initialize();
